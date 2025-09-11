1581bc49bc92fcb88157a24a6644c94cc029f5b7
sfauzii <sjahrulfauzii@gmail.com>
2025-09-11 11:03:48 +0700

    refactor: add wrapper step mobile version

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -377,6 +377,100 @@ const cards = [
             </div>
         </section>
 
+        <section class="px-6 mb-10 lg:hidden">
+            <!-- Kanan -->
+            <div>
+                <h3 class="text-lg font-semibold text-abu mb-6">Apa yang bisa kita lakukan?</h3>
+                <!-- Wrapper untuk step -->
+                <div class="relative pl-6 space-y-8">
+                    <!-- Vertical dashed line -->
+                    <div
+                        class="absolute top-0 bottom-0 w-px border-l-2 border-dashed border-blue-200/70"
+                    ></div>
+
+                    <!-- Step 1 -->
+                    <div class="relative flex items-start">
+                        <!-- Number step -->
+                        <div
+                            class="absolute bottom-6 -translate-x-1/2 flex items-center justify-center w-12 h-12 rounded-full bg-white z-10"
+                        >
+                            <div
+                                class="flex items-center justify-center w-6 h-6 text-[13px] rounded-full bg-blue-donker text-white font-medium"
+                            >1</div>
+                        </div>
+
+                        <!-- Card -->
+                        <div
+                            class="ml-8 flex items-center bg-white rounded-2xl shadow-sm p-4 hover:shadow-md transition w-full"
+                        >
+                            <img src="/a1.avif" alt="Pantau Janji" class="w-16 h-16 flex-shrink-0" />
+                            <div class="ml-4 flex-1">
+                                <h4 class="font-bold text-blue-donker">Pantau janjinya</h4>
+                                <p
+                                    class="text-sm text-gray-500"
+                                >Nge-track misi, proker, quick wins yang dijanjiin Presiden</p>
+                            </div>
+                            <Icon name="mdi:chevron-right" class="w-6 h-6 text-gray-400" />
+                        </div>
+                    </div>
+
+                    <!-- Step 2 -->
+                    <div class="relative flex items-start">
+                        <div
+                            class="absolute bottom-6 -translate-x-1/2 flex items-center justify-center w-12 h-12 rounded-full bg-white z-10"
+                        >
+                            <div
+                                class="flex items-center justify-center w-6 h-6 text-[13px] rounded-full bg-blue-donker text-white font-medium"
+                            >2</div>
+                        </div>
+                        <div
+                            class="ml-8 flex items-center bg-white rounded-2xl shadow-sm p-4 hover:shadow-md transition w-full"
+                        >
+                            <img
+                                src="/a2.avif"
+                                alt="Pantau Kebijakan"
+                                class="w-16 h-16 flex-shrink-0"
+                            />
+                            <div class="ml-4 flex-1">
+                                <h4 class="font-bold text-blue-donker">Pantau kebijakannya</h4>
+                                <p
+                                    class="text-sm text-gray-500"
+                                >Pelajari dokumennya, lalu beri masukan teknis dan pandanganmu</p>
+                            </div>
+                            <Icon name="mdi:chevron-right" class="w-6 h-6 text-gray-400" />
+                        </div>
+                    </div>
+
+                    <!-- Step 3 -->
+                    <div class="relative flex items-start">
+                        <div
+                            class="absolute bottom-6 -translate-x-1/2 flex items-center justify-center w-12 h-12 rounded-full bg-white z-10"
+                        >
+                            <div
+                                class="flex items-center justify-center w-6 h-6 text-[13px] rounded-full bg-blue-donker text-white font-medium"
+                            >3</div>
+                        </div>
+                        <div
+                            class="ml-8 flex items-center bg-white rounded-2xl shadow-sm p-4 hover:shadow-md transition w-full"
+                        >
+                            <img
+                                src="/a1.avif"
+                                alt="Pantau Pejabat"
+                                class="w-16 h-16 flex-shrink-0"
+                            />
+                            <div class="ml-4 flex-1">
+                                <h4 class="font-bold text-blue-donker">Pantau pejabatnya</h4>
+                                <p
+                                    class="text-sm text-gray-500"
+                                >Cek siapa yang paling bertanggung jawab untuk masalah kamu</p>
+                            </div>
+                            <Icon name="mdi:chevron-right" class="w-6 h-6 text-gray-400" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
         <!-- Section Participation Desktop -->
         <section class="max-w-6xl mx-auto px-6 py-4 relative mt-5 hidden md:flex">
             <div
