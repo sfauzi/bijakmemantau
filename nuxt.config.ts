798fbe964999423eb3cfc83cf479bceadebd66f2
sfauzii <sjahrulfauzii@gmail.com>
2025-09-11 12:33:57 +0700

    refactor: deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  ssr: false,
+  nitro: {
+    preset: "static"
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/google-fonts',
